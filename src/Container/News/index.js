bc333d60e3ecf63185afe87e803874ce71e30988
Caleb335 <belac335@gmail.com>
2020-12-03 11:57:07 +0100

    fix layour on large screen

--- src/Container/News/index.js
+++ src/Container/News/index.js
@@ -3,12 +3,37 @@ import news from "./style/news.module.css"
 import { liveSports, headlines } from "../../news_data"
 import { Container, Row, Col } from "reactstrap"
 import Card from "../../../components/card"
+import { IoIosArrowForward } from "react-icons/io"
 
 const NewsLayout = () => {
   return (
     <Container>
-        <section className={news.news_sect}>
-        <h1>News Layout</h1>
+      <section className={news.news_sect}>
+        <div className={news.banner}>
+          <div className={news.banner_content}>
+            <img
+              src="/img/anthony-joshua.jpeg"
+              alt="anthony joshua"
+              className={news.img}
+            />
+            <div className={news.banner_details}>
+              <div>
+                <div className={news.contestants}>
+                <p className={news.banner_sport}>Boxing</p> 
+                  <h5>AJ Vs Pulev in front of 1,000 fans</h5>
+                </div>
+                <div className={news.bookings}>
+                  <p>
+                    Book Joshua VS Pulev now! <IoIosArrowForward />
+                  </p>
+                  <p>
+                    Joshua VS Pulev: Booking info <IoIosArrowForward />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <Row>
           {headlines.map((headline, index) => {
             return (
@@ -25,12 +50,35 @@ const NewsLayout = () => {
                   </div>
                 </div>
               </Card>
-            )
+            );
           })}
         </Row>
-    </section>
+        <section className={news.live}>
+          <h3>Live on Sky</h3>
+          <Row>
+            {liveSports.map((sports, index) => {
+              return (
+                <div key={index} id={news.card}>
+                  <div className={news.live_content}>
+                    <p className={news.sport}>{sports.name}</p>
+                    <h4>{sports.title}</h4>
+                    <p>{sports.status}</p>
+                    <img src={sports.img} />
+                    <p className={news.muted}>
+                      Get Sky Sports <IoIosArrowForward />{" "}
+                    </p>
+                    <p className={news.mute}>
+                      Get a Sky Sports Pass <IoIosArrowForward />{" "}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </Row>
+        </section>
+      </section>
     </Container>
-  )
-}
+  );
+};
 
 export default NewsLayout;
--- src/news_data.js
+++ src/news_data.js
@@ -2,46 +2,46 @@ export const headlines = [
   {
     sport: "Formula 1",
     details: "Russel replace Hamilton at Mercedes fro Sakhir GP",
-    img: "/img/russell.jpeg"
+    img: "/img/russell.jpeg",
   },
   {
     sport: "Football",
     details: "Alisson out for up to two weeks; Klopp Kelleher's verdict",
-    img: "/img/klopp.jpeg"
+    img: "/img/klopp.jpeg",
   },
   {
     sport: "Formula 1",
     details: "Mich Schumacher joining F1 grid with Haas fro 2021",
-    img: "/img/schumacher.jpeg"
+    img: "/img/schumacher.jpeg",
   },
   {
     sport: "Cricket",
     details: "More to come from 'exceptionally strong' England",
-    img: "/img/jordan.jpeg"
+    img: "/img/jordan.jpeg",
   },
   {
     sport: "Darts",
     details: "PDC confirms fans to attend World Dart Championship",
-    img: "/img/dart.jpeg"
-  }, 
+    img: "/img/dart.jpeg",
+  },
   {
     sport: "Football",
     details: "Tuchel: Rashford becoming 'annoying' for PSG",
-    img: "/img/rashford.jpeg"
-  }
+    img: "/img/rashford.jpeg",
+  },
 ]
 
 export const liveSports = [
-    {
-        name: "Golf",
-        title: "Golf in Dubai Championship",
-        status: "Live Now",
-        img: "/img/Sky-Sports-Logo.svg"
-    }, 
-    {
-        name: "Racing",
-        title: "Raceday Live",
-        status: "12pm",
-        img: "/img/Sky-Sports-Logo.svg"
-    }
+  {
+    name: "Golf",
+    title: "Golf in Dubai Championship",
+    status: "Live Now",
+    img: "/img/Sky-Sports-Logo.svg",
+  },
+  {
+    name: "Racing",
+    title: "Raceday Live",
+    status: "12pm",
+    img: "/img/Sky-Sports-Logo.svg",
+  },
 ]
