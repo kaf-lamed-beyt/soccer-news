5b1b22d8f5054cc2563231933c1c234b4a74dbcd
Caleb335 <belac335@gmail.com>
2020-12-03 08:12:23 +0100

    add card images

--- src/Container/News/index.js
+++ src/Container/News/index.js
@@ -1,11 +1,24 @@
 import React from "react"
 import news from "./style/news.module.css"
 import { liveSports, headlines } from "../../news_data"
+import { Container, Row, Col } from "reactstrap"
+import Card from "../../../components/card"
 
 const NewsLayout = () => {
     return (
         <section className={news.news_sect}>
+            <Container>
             <h1>News Layout</h1>
+            <Row>
+                {headlines.map((headline, index) => {
+                    return (
+                        <Card key={index}>
+                            
+                        </Card>
+                    )
+                })}
+            </Row>
+            </Container>
         </section>
     )
 }
--- src/news_data.js
+++ src/news_data.js
@@ -1,41 +1,47 @@
-export default headlines = [
+export const headlines = [
   {
     sport: "Formula 1",
     details: "Russel replace Hamilton at Mercedes fro Sakhir GP",
+    img: "/img/russell.jpeg"
   },
   {
     sport: "Football",
     details: "Alisson out for up to two weeks; Klopp Kelleher's verdict",
+    img: "/img/klopp.jpeg"
   },
   {
     sport: "Formula 1",
     details: "Mich Schumacher joining F1 grid with Haas fro 2021",
+    img: "schumacher.jpeg"
   },
   {
     sport: "Cricket",
-    details: "More to come from 'exceptionally strong' England"
+    details: "More to come from 'exceptionally strong' England",
+    img: "/img/jordan.jpeg"
   },
   {
     sport: "Darts",
-    details: "PDC confirms fans to attend World Dart Championship"
+    details: "PDC confirms fans to attend World Dart Championship",
+    img: "/img/dart.jpeg"
   }, 
   {
     sport: "Football",
-    details: "Tuchel: Rashford becoming 'annoying' for PSG"
+    details: "Tuchel: Rashford becoming 'annoying' for PSG",
+    img: "/img/rashford.jpeg"
   }
 ]
 
-export default liveSports = [
+export const liveSports = [
     {
         name: "Golf",
         title: "Golf in Dubai Championship",
         status: "Live Now",
-        img: ""
+        img: "/img/Sky-Sports-Logo.svg"
     }, 
     {
         name: "Racing",
         title: "Raceday Live",
         status: "12pm",
-        img: ""
+        img: "/img/Sky-Sports-Logo.svg"
     }
 ]
