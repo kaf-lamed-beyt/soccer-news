bde068baef29969ed326f4f29efd1607f62ba807
Caleb335 <belac335@gmail.com>
2020-12-07 22:09:35 +0100

    fix layout on mobile screen (small) --again

--- src/Container/News/index.js
+++ src/Container/News/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import news from "./style/news.module.css";
 import { liveSports, headlines } from "../../news_data";
-import { Row, Col } from "reactstrap";
 import Card from "../../../components/card";
 import { IoIosArrowForward } from "react-icons/io";
-import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Banner from "../../Layout/Banner";
 
@@ -13,18 +11,18 @@ const NewsLayout = () => {
     <Grid>
       <section className={news.news_sect}>
         <Banner />
-        <Grid container >
-            {headlines.map((headline, index) => {
-              return (
-                <Grid item xs={12} lg={4} md={6} >
-                  <Card key={index} className={news.card_item}>
+        <Grid container>
+          {headlines.map((headline, index) => {
+            return (
+              <Grid item xs={12} lg={4}>
+                <Card key={index} className={news.card_item}>
                   <div className={news.content_area}>
                     <div className={news.img_cont}>
-                    <img
-                      src={headline.img}
-                      alt="headline image"
-                      className={news.headline_img}
-                    />
+                      <img
+                        src={headline.img}
+                        alt="headline image"
+                        className={news.headline_img}
+                      />
                     </div>
                     <div className={news.details}>
                       <p className={news.sport}>{headline.sport}</p>
@@ -32,33 +30,33 @@ const NewsLayout = () => {
                     </div>
                   </div>
                 </Card>
-                </Grid>
-              )
-            })}
+              </Grid>
+            );
+          })}
         </Grid>
         <Grid container>
           <section className={news.live}>
             <h3>Live on Sky</h3>
-              {liveSports.map((sports, index) => {
-                return (
-                  <div key={index} id={news.card}>
-                    <div className={news.live_content}>
-                      <p className={news.sport}>{sports.name}</p>
-                      <h4>{sports.title}</h4>
-                      <p>{sports.status}</p>
-                      <img src={sports.img} />
-                      <div className={news.muted_txt}>
-                        <p className={news.muted}>
-                          Get Sky Sports <IoIosArrowForward />{" "}
-                        </p>
-                        <p className={news.mute}>
-                          Get a Sky Sports Pass <IoIosArrowForward />{" "}
-                        </p>
-                      </div>
+            {liveSports.map((sports, index) => {
+              return (
+                <div key={index} id={news.card}>
+                  <div className={news.live_content}>
+                    <p className={news.sport}>{sports.name}</p>
+                    <h4>{sports.title}</h4>
+                    <p>{sports.status}</p>
+                    <img src={sports.img} />
+                    <div className={news.muted_txt}>
+                      <p className={news.muted}>
+                        Get Sky Sports <IoIosArrowForward />{" "}
+                      </p>
+                      <p className={news.mute}>
+                        Get a Sky Sports Pass <IoIosArrowForward />{" "}
+                      </p>
                     </div>
                   </div>
-                );
-              })}
+                </div>
+              );
+            })}
           </section>
         </Grid>
       </section>
--- src/Layout/Banner/index.js
+++ src/Layout/Banner/index.js
@@ -6,32 +6,32 @@ import { IoIosArrowForward } from "react-icons/io";
 const Banner = () => {
   return (
     <Grid container xs={12} lg={12} md={12}>
-    <div className={banner.banner}>
-      <div className={banner.banner_content}>
-        <img
-          src="/img/anthony-joshua.jpeg"
-          alt="anthony joshua"
-          className={banner.img}
-        />
-        <div className={banner.banner_details}>
-          <div>
-            <div className={banner.contestants}>
-              <p className={banner.banner_sport}>Boxing</p>
-              <h5>AJ Vs Pulev in front of 1,000 fans</h5>
-            </div>
-            <div className={banner.bookings}>
-              <p>
-                Book Joshua VS Pulev now! <IoIosArrowForward />
-              </p>
-              <p>
-                Joshua VS Pulev: Booking info <IoIosArrowForward />
-              </p>
+      <div className={banner.banner}>
+        <div className={banner.banner_content}>
+          <img
+            src="/img/anthony-joshua.jpeg"
+            alt="anthony joshua"
+            className={banner.img}
+          />
+          <div className={banner.banner_details}>
+            <div>
+              <div className={banner.contestants}>
+                <p className={banner.banner_sport}>Boxing</p>
+                <h5>AJ Vs Pulev in front of 1,000 fans</h5>
+              </div>
+              <div className={banner.bookings}>
+                <p>
+                  Book Joshua VS Pulev now! <IoIosArrowForward />
+                </p>
+                <p>
+                  Joshua VS Pulev: Booking info <IoIosArrowForward />
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>  
-    </div>  
-    </Grid>    
+      </div>
+    </Grid>
   );
 };
 
